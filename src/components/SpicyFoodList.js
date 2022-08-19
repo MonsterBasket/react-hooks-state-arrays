718bed2d088a7d519b22947776f53d6edda29da2
MonsterBasket <blazza1@hotmail.com>
2022-08-19 14:27:25 +1000

    that took longer than I expected

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,16 +3,59 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  // const allFoods = [...foods]; //doesn't work, each time you re-render the screen it refreshes the script, so allFoods will always be blasted and match foods.
+  const [filterBy, setFilterBy] = useState("All")
 
-  function handleAddFood() {
-    const newFood = getNewSpicyFood();
-    console.log(newFood);
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All"){
+      return true;
+    }
+    else{
+      return food.cuisine === filterBy;
+    }
+  })
+
+  function addFood() {
+    if (foods.length === 5) return; //hard coded limit because getNewSpicyFood() is a dumb function.
+    const newFoods = [...foods, getNewSpicyFood()];
+    setFoods(newFoods);
+  }
+
+  function removeItem(id){
+    setFoods(foods.filter(item => item.id !== id));
+    console.log(foods.filter(item => item.id !== id))
+  }
+
+  function increaseHeat(id){
+    const updatedFoods = [...foods]
+    updatedFoods.find(a => a.id === id).heatLevel ++;
+    setFoods(updatedFoods);
+  }
+
+  // function setFilter(e){
+  //   let updatedFoods = [...allFoods];
+  //   if (e.target.value !== "All"){
+  //     updatedFoods = allFoods.filter(item => item.cuisine === e.target.value)
+  //   }
+  //     setFilterBy(updatedFoods);  
+  // }
+  function setFilter(e){
+    setFilterBy(e.target.value);
   }
 
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <button onClick={addFood}>Add New Food</button>
+      <select name="filter" onChange={setFilter}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+      <ul>{
+        foodsToDisplay.map(item => <li key={item.id} id={item.id} onClick={_ => /*removeItem(item.id)*/ increaseHeat(item.id)}>{`${item.name} - ${item.cuisine} ${"🌶️".repeat(item.heatLevel)}`}</li>)
+      }</ul>
     </div>
   );
 }
